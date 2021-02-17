9ed95fa8247cf8a2f5bf33b255b1477f6e1f09c4
Almamma Bashar <basharmamma@hotmail.com>
2021-02-17 15:12:23 +0300

    Initial commit

---
+++ AntiVirus/DBConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace AntiVirus
+{
+    class DBConnector
+    {
+        SqliteConnection connection;
+
+        DBConnector(String source)
+        {
+            var connection = new SqliteConnection("Data Source=" + source);
+            connection.Open();
+        }
+    }
+}
